3b0f4d3397d722cdb99ba08ef0597803271af70d
Matthew Duffy <mattduffy@gmail.com>
2023-08-17 22:19:12 -0700

    Add login related view tempates, robots.txt and sitemap.xml files.

--- public/j/orientation.js
+++ public/j/orientation.js
@@ -1,20 +1,22 @@
-const deviceOrientationPermissionButton = document.createElement('button')
-deviceOrientationPermissionButton.innerText = 'Can I please?'
-const permButtonDiv = document.querySelector('div#permissionButton')
-permButtonDiv.appendChild(deviceOrientationPermissionButton)
-if (deviceOrientationPermissionButton !== null) {
-  window.addEventListener('touchend', async (e) => {
-    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-      const permissionIs = await DeviceOrientationEvent.requestPermission()
-      console.log(`DeviceOrientationEvent persmission is ${permissionIs}`)
-      if (permissionIs === 'granted') {
-        window.addEventListener('deviceorientation', (event) => {
-          // console.log(`${event.alpha} : ${event.beta} : ${event.gamma}`)
-        })
+const permButtonDiv = document.querySelector('div#permissionButton') ?? null
+if (permButtonDiv !== null) {
+  permButtonDiv.appendChild(deviceOrientationPermissionButton)
+  const deviceOrientationPermissionButton = document.createElement('button')
+  deviceOrientationPermissionButton.innerText = 'Can I please?'
+  if (deviceOrientationPermissionButton !== null) {
+    window.addEventListener('touchend', async (e) => {
+      if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+        const permissionIs = await DeviceOrientationEvent.requestPermission()
+        console.log(`DeviceOrientationEvent persmission is ${permissionIs}`)
+        if (permissionIs === 'granted') {
+          window.addEventListener('deviceorientation', (event) => {
+            // console.log(`${event.alpha} : ${event.beta} : ${event.gamma}`)
+          })
+        }
+      } else {
+        console.error('No device orientation available.')
       }
-    } else {
-      console.error('No device orientation available.')
-    }
-    console.info(e)
-  })
+      console.info(e)
+    })
+  }
 }
