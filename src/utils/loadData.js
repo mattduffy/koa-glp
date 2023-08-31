2f7413861ab20eb29cf0ee6faed4e8df49ddd8bd
Matthew Duffy <mattduffy@gmail.com>
2023-08-30 21:11:31 -0700

    Add loadData.js script to the npm rum commands in package.json file.

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -5,13 +5,12 @@
  */
 
 import path from 'node:path'
+import { opendir, readdir, readFile } from 'node:fs/promises'
 import * as dotenv from 'dotenv'
 import { fileURLToPath } from 'node:url'
-import crypto from 'node:crypto'
-/* eslint-disable-next-line */
 import { Command } from 'commander'
-import * as mongoClient from '../daos/impl/mongodb/mongo-client.js'
-import * as redis from '../daos/impl/redis/redis-client.js'
+// import * as mongoClient from '../daos/impl/mongodb/mongo-client.js'
+// import * as redis from '../daos/impl/redis/redis-client.js'
 import { _log, _error } from './logging.js'
 
 const log = _log.extend('utils:load-data')
@@ -29,37 +28,52 @@ dotenv.config({ path: path.resolve(appRoot, 'config/app.env'), processEnv: appEn
 // log(mongoEnv)
 
 const program = new Command()
-program.name('newUser')
-  .requiredOption('')
-  .requiredOption('')
-  .requiredOption('')
-  .requiredOption('')
+program.name('loadData')
+  .requiredOption('--data-dir <dir>', 'Directory containing JSON data files to load.', 'data/')
+  .option('--batch-size <size>', 'Number of records to load in Redis pipeline.  Default is 25', 25)
+  .option('--key-prefix <prefix>', 'The key prefix for Redis to use to namespace loaded data.', 'loadtest')
 
 program.parse(process.argv)
 const options = program.opts()
 log(options)
 
-const ctx = {
-  app: {
-    root: appRoot,
-    dirs: {
-      public: {
-        dir: `${appRoot}/public`,
-      },
-      private: {
-        dir: `${appRoot}/private`,
-      },
-    },
-  },
+const dataDir = path.resolve(appRoot, options.dataDir)
+let subDirs
+try {
+  subDirs = await readdir(dataDir)
+  log(subDirs)
+  // subDirs.forEach(async (d, i) => {
+  /* eslint-disable no-restricted-syntax */
+  for await (const d of subDirs) {
+    // const n = i + 1
+    const pierDir = path.resolve(dataDir, d)
+    log(`pier dir: ${pierDir}`)
+    const dir = await opendir(path.resolve(dataDir, d), { withFileTypes: true })
+    log(dir)
+  }
+  /* eslint-enable no-restricted-syntax */
+} catch (e) {
+  error(e)
+  throw new Error(e.message, { cause: e })
 }
 
-
+// const ctx = {
+//   app: {
+//     root: appRoot,
+//     dirs: {
+//       public: {
+//         dir: `${appRoot}/public`,
+//       },
+//       private: {
+//         dir: `${appRoot}/private`,
+//       },
+//     },
+//   },
+// }
 
 // log(mongoClient.uri)
 // log('[newUser] DB credentials in use: %O', userProps.client.options.credentials)
 // log('[newUser] DB name in use: ', userProps.client.options.dbName)
-
-
 
 // Done loading the pier data, exit process.
 process.exit()
