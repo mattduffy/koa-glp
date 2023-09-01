be967b343ef7407dc201547713b120a93edc1fed
Matthew Duffy <mattduffy@gmail.com>
2023-08-31 22:38:07 -0700

    Working on loadData.js script.

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -44,13 +44,40 @@ try {
   log(subDirs)
   // subDirs.forEach(async (d, i) => {
   /* eslint-disable no-restricted-syntax */
+  let ttlGrand = 0
+  const ttlCounts = {}
   for await (const d of subDirs) {
-    // const n = i + 1
     const pierDir = path.resolve(dataDir, d)
     log(`pier dir: ${pierDir}`)
     const dir = await opendir(path.resolve(dataDir, d), { withFileTypes: true })
-    log(dir)
+    let ttlCounter = 0
+    let counter = 0
+    let buffer = []
+    let pier
+    while ((pier = await dir.read()) !== null) {
+      log(pier.name)
+      buffer.push(pier.name)
+      counter += 1
+      ttlCounter += 1
+      // if (counter <= options.batchSize) {
+      if (buffer.length >= options.batchSize) {
+        log(`buffer.length ${buffer.length} >= batchSize: ${options.batchSize}`)
+        log(buffer)
+        log('stuff the buffer into the radis pipeline.')
+        buffer = []
+        counter = 0
+      }
+    }
+    if (buffer.length > 0) {
+      log('process remaining partial batch of pier files.')
+      log(buffer)
+      buffer = []
+    }
+    ttlCounts[d] = ttlCounter
+    ttlGrand += ttlCounter
   }
+  log(`Grand Total number of pier files processed: ${ttlGrand}`)
+  log(ttlCounts)
   /* eslint-enable no-restricted-syntax */
 } catch (e) {
   error(e)
