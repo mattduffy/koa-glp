7911fab0554c8185bd312a60a8a0ddb7e42567f1
Matthew Duffy <mattduffy@gmail.com>
2023-09-08 20:55:55 -0700

    Add scan of keys to be deleted from redis.

--- src/utils/deleteData.js
+++ src/utils/deleteData.js
@@ -28,20 +28,73 @@ const DB_PREFIX = redisEnv.REDIS_KEY_PREFIX
 
 const program = new Command()
 program.name('loadData')
-  .requiredOption('--database <dir>', 'Database to delete data from.')
+  // .requiredOption('--database <num>', 'Redis database to delete keys from.')
   .requiredOption('--key-prefix <prefix>', 'The app-specific key prefix for Redis to use.')
   .requiredOption('--key-name <name>', 'The key name for Redis to append to the app-specific key prefix.')
 
 program.parse(process.argv)
 const options = program.opts()
+options.dbPrefix = DB_PREFIX
 log(options)
 
-const appPrefix = (options.keyPrefix !== undefined) ? `:${options.keyPrefix}` : ''
-const prefix = `${DB_PREFIX}${appPrefix}:${options.keyName}`
-log(`appPrefix: ${appPrefix}`)
-log(`prefix: ${prefix}`)
+const keyPath = (DB_PREFIX !== options.keyPrefix) ? `${DB_PREFIX}:${options.keyPrefix}:${options.keyName}:*` : `${options.keyPrefix}:${options.keyName}:*`
+log(`prefix: ${keyPath}`)
 
+async function del() {
+  return new Promise((resolve, reject) => {
+    let deletedKeys = 0
+    const scanArgs = {
+      match: keyPath,
+      type: 'ReJSON-RL',
+      count: 20,
+    }
+    log(scanArgs)
+    const stream = redis.scanStream(scanArgs)
+    stream.on('data', (keys) => {
+      log(keys.length)
+      if (keys.length > 0) {
+        keys.forEach((key) => {
+          log(key)
+          deletedKeys += 1
+        })
+      } else {
+        log(`no keys found matching ${keyPath}`)
+      }
+    })
+    stream.on('end', () => {
+      log(`Deleted ${deletedKeys} from ${keyPath}`)
+      resolve()
+    })
+  })
+}
+await del()
 
+// try {
+//  let deletedKeys = 0
+//  const scanArgs = {
+//    match: keyPath,
+//    type: 'ReJSON-RL',
+//    count: 30,
+//  }
+//  log(scanArgs)
+//  const stream = await redis.scanStream(scanArgs)
+//  log(stream)
+//  stream.on('data', (resultKeys) => {
+//    stream.pause()
+//    log(resultKeys)
+//    resultKeys.forEach((key) => {
+//      log(key)
+//      deletedKeys += 1
+//    })
+//    stream.resume()
+//  })
+//  stream.on('end', () => {
+//    log(`Deleted ${deletedKeys} from ${keyPath}`)
+//  })
+// } catch (e) {
+//   error(e)
+//   throw new Error(e.message, { cause: e })
+// }
 
 // Done deleting the data, exit process.
 process.exit()
