98a1720d3d654efefd84d8c856d30c53d2f6ec8c
Matthew Duffy <mattduffy@gmail.com>
2023-09-10 19:28:08 -0700

    Update the loadData.js script to guarantee the piers get loaded in ascending order.

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -1,11 +1,15 @@
 /**
  * @module @mattduffy/koa-glp
  * @author Matthew Duffy <mattduffy@gmail.com>
- * @file src/utils/loadData.js The script to load pier data into the database.
+ * @file src/utils/loadData.js The script to load pier data into redis.
  */
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'node:path'
-import { opendir, readdir, readFile } from 'node:fs/promises'
+import {
+  // opendir,
+  readdir,
+  readFile,
+} from 'node:fs/promises'
 import * as dotenv from 'dotenv'
 import { fileURLToPath } from 'node:url'
 import { Command } from 'commander'
@@ -18,7 +22,6 @@ import {
   Schema,
   Repository,
 } from '../daos/impl/redis/redis-om.js'
-// } from '../daos/impl/redis/redis-client.js'
 import { _log, _error } from './logging.js'
 /* eslint-enable import/no-extraneous-dependencies */
 
@@ -45,10 +48,16 @@ program.name('loadData')
 program.parse(process.argv)
 const options = program.opts()
 log(options)
+log(clientOm)
 
-const appPrefix = (options.keyPrefix !== undefined) ? `:${options.keyPrefix}` : ''
-const prefix = `${DB_PREFIX}${appPrefix}:${options.keyName}`
-log(`appPrefix: ${appPrefix}`)
+const transparentKeyPrefix = redis?.options?.keyPrefix
+let prefix
+if (transparentKeyPrefix === null || transparentKeyPrefix === undefined || transparentKeyPrefix === '') {
+  prefix = `${DB_PREFIX}:${options.keyName}`
+} else {
+  prefix = `${options.keyName}`
+}
+
 log(`prefix: ${prefix}`)
 const pierSchema = new Schema(prefix, {
   pier: { type: 'string', path: '$.pier' },
@@ -66,42 +75,8 @@ const pierSchema = new Schema(prefix, {
 }, {
   dataStructure: 'JSON',
 })
-
 const pierRepository = new Repository(pierSchema, clientOm)
-// const pier001 = {
-//   pier: '001',
-//   loc: { longitude: -88.442671, latitude: 42.590727 },
-//   // loc: ['-88.442671,42.590727'],
-//   geohash: 'dp9473qhbq0',
-//   pluscode: 'HHR4+7WW Lake Geneva, Wisconsin',
-//   property: {
-//     tel: '(123) 456-7890',
-//     address: {
-//       street: '1224 W Main St',
-//       city: 'Lake Geneva',
-//       state: 'WI',
-//       zip: '53147',
-//     },
-//   },
-//   owners: [
-//     {
-//       estateName: '',
-//       member: true,
-//       membershipType: 'Sustaining',
-//       members: [
-//         {
-//           t: '', f: 'Alan', m: '', l: 'Bosworth', s: '',
-//         },
-//         {
-//           t: '', f: 'Kathi', m: '', l: 'Bosworth', s: '',
-//         },
-//       ],
-//     },
-//   ],
-// }
-// const saved = await pierrepository.save(pier001.pier, pier001)
-// log(saved)
-// process.exit()
+
 // create the indexes for pier data
 const pierNumberIndex = `${DB_PREFIX}:idx:piers:number`
 try {
@@ -156,23 +131,20 @@ try {
 const dataDir = path.resolve(appRoot, options.dataDir)
 let subDirs
 try {
-  subDirs = await readdir(dataDir)
+  subDirs = (await readdir(dataDir)).sort()
   log(subDirs)
-  /* eslint-disable no-restricted-syntax */
   let ttlGrand = 0
   let missingLocCounter = 0
   const ttlCounts = {}
+  /* eslint-disable-next-line */
   for await (const d of subDirs) {
     const pierDir = path.resolve(dataDir, d)
     log(`pier dir: ${pierDir}`)
-    const dir = await opendir(path.resolve(dataDir, d), { withFileTypes: true })
+    const dir = (await readdir(pierDir)).sort()
     let ttlCounter = 0
-    let pier
     /* eslint-disable-next-line */
-    while ((pier = await dir.read()) !== null) {
-      // log(path.resolve(dataDir, d, pier.name))
-      /* eslint-disable no-await-in-loop */
-      let pierJson = await readFile(path.resolve(dataDir, d, pier.name), 'utf-8')
+    for await (const pier of dir) {
+      let pierJson = await readFile(path.resolve(dataDir, d, pier), 'utf-8')
       pierJson = pierJson.replace(/\n/g, '')
       pierJson = JSON.parse(pierJson)
       pierJson.owners.forEach((owner, i) => {
@@ -201,7 +173,6 @@ try {
       const saved = await pierRepository.save(pierJson.pier, pierJson)
       log(saved)
       ttlCounter += 1
-      // if (counter <= options.batchSize) {
     }
     ttlCounts[d] = ttlCounter
     ttlGrand += ttlCounter
