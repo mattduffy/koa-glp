23c026061391662fc48f933ce023bff0dd2a7efb
Matthew Duffy <mattduffy@gmail.com>
2023-10-18 21:44:26 -0700

    Add ajax route for requesting all pier number/coords for making make markers and moving maker annotation code from mapkit-embed.ejs template to each individual page.

--- src/routes/mapkit.js
+++ src/routes/mapkit.js
@@ -23,6 +23,47 @@ function sanitize(param) {
   return param
 }
 
+router.get('allPiers', '/mapkit/allPiers', async (ctx) => {
+  const log = Log.extend('allPiers')
+  const info = Info.extend('allPiers')
+  const error = Error.extend('allPiers')
+  if (ctx.state.isAsyncRequest === true) {
+    log('Async query received.')
+  }
+  info('Getting all piers to be displayed on map as geocoded annotations.')
+  let allPiers
+  try {
+    const idxAllPiers = 'glp:idx:piers:number'
+    const queryAllPiers = '*'
+    const optsAllPiers = {}
+    optsAllPiers.RETURN = ['pierNumber', '$.loc']
+    optsAllPiers.SORTBY = { BY: 'pierNumber', DIRECTION: 'ASC' }
+    optsAllPiers.LIMIT = { from: 0, size: 1000 }
+    log(`ft.search ${idxAllPiers} "*" RETURN 4 pierNumber $.loc AS coords SORTBY pierNumber ASC LIMIT 0 1000`)
+    allPiers = await redis.ft.search(idxAllPiers, queryAllPiers, optsAllPiers)
+    log(allPiers)
+  } catch (e) {
+    error('Failed getting all piers from redis.')
+    error(e)
+    // throw new Error('Failed getting all piers from redis', { cause: e })
+    allPiers = { error: 'Failed getting all piers.', total: 0, documents: [] }
+  }
+  const csrfTokenCookie = ctx.cookies.get('csrfToken')
+  const csrfTokenSession = ctx.session.csrfToken
+  info(`${csrfTokenCookie},\n${csrfTokenSession}`)
+  if (csrfTokenCookie === csrfTokenSession) info('cookie === session')
+  if (!(csrfTokenCookie === csrfTokenSession)) {
+    error(`CSR-Token mismatch: header:${csrfTokenCookie} - session:${csrfTokenSession}`)
+    ctx.type = 'application/json; charset=utf-8'
+    ctx.status = 401
+    ctx.body = { error: 'csrf token mismatch' }
+  } else {
+    ctx.type = 'application/json; charset=utf-8'
+    ctx.status = 200
+    ctx.body = allPiers
+  }
+})
+
 router.get('mapkitGetToken', '/mapkit/getToken', async (ctx) => {
   const log = Log.extend('get-token')
   const info = Info.extend('get-token')
