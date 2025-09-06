2bb609a207ab404ec1439ee54da7b911e39fb5fc
Matthew Duffy <mattduffy@gmail.com>
2025-09-05 21:20:52 -0700

    Remove lingering references to ioredis.

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -12,7 +12,7 @@ import crypto from 'node:crypto'
 /* eslint-disable-next-line */
 import * as Users from '@mattduffy/users/Users.js'
 import * as mongoClient from '../daos/impl/mongodb/mongo-client.js'
-// import { ioredis } from '../daos/impl/redis/ioredis-client.js'
+// import { redis } from '../daos/impl/redis/redis-single.js'
 import { App } from '../models/app.js'
 // import { Users } from '../models/users.js'
 import { _log, _error } from './logging.js'
