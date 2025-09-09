5e3b2a09da4660fa29f11590a0d37805b505b205
Matthew Duffy <mattduffy@gmail.com>
2025-09-08 21:12:32 -0700

    create functions to drop and create new vector index.  Tweek the text of candidate embeddings to place standardized (looking) tokens for missing field values.

--- src/utils/createVectorSearchIndex.js
+++ src/utils/createVectorSearchIndex.js
@@ -7,6 +7,12 @@
 import path from 'node:path'
 import * as dotenv from 'dotenv'
 import { fileURLToPath } from 'node:url'
+import * as transformers from '@xenova/transformers'
+import {
+  SCHEMA_VECTOR_FIELD_ALGORITHM,
+  createClient,
+  SCHEMA_FIELD_TYPE,
+} from 'redis'
 import { Command } from 'commander'
 import { redis_single as redis } from '../daos/impl/redis/redis-single.js'
 import { _log, _error } from './logging.js'
@@ -29,6 +35,19 @@ dotenv.config({
   processEnv: redisEnv,
 })
 const DB_PREFIX = redisEnv.REDIS_KEY_PREFIX
+const aiEnv = {}
+dotenv.config({
+  path: path.resolve(appRoot, 'config/ai.env'),
+  processEnv: aiEnv,
+})
+log('aiEnv', aiEnv)
+transformers.env.localModelPath = path.resolve(appRoot, aiEnv.MODEL_CACHE_DIR)
+// transformers.env.allowRemoteModels = false
+let pipeline = transformers.pipeline(aiEnv.EMBEDDING_TASK, aiEnv.EMBEDDING_MODEL)
+const pipeOptions = {
+  pooling: 'mean',
+  normalize: true,
+}
 
 const program = new Command()
 program.name('deleteData')
@@ -46,13 +65,62 @@ let keyPrefix
 if (options.keyPrefix.slice(-1) !== ':') {
   options.keyPrefix += ':'
 }
-keyPrefix = `${options.keyPrefix}*`
+keyPrefix = `${options.keyPrefix}`
+const keyPrefixStar = keyPrefix + '*'
 // process.exit()
 
-async function del() {
+async function deleteVectorIndex(idxName) {
+  let result
+  log('droping vector index (if it exists): ', idxName)
+  try {
+    result = await redis.ft.dropIndex(idxName) 
+  } catch (e) {
+    log(`Redis responded to ft.dropIndex(${idxName}) with: ${e.message}`)
+    // throw new Error('Redis Drop Index failed.', { cause: e })
+  }
+  return result
+}
+
+async function createVectorIndex(idxName) {
+  const pierVectorIndex = `${DB_PREFIX}:idx:vector:${idxName}`
+  log('creating vector index:', pierVectorIndex)
+  const pierEmbeddingsPrefix = `${keyPrefix}embeddings:`
+  log('referencing embedding keys with prefix: ', pierEmbeddingsPrefix)
+  let result
+  try {
+    result = await redis.ft.create(pierVectorIndex, {
+      '$.content': {
+        type: SCHEMA_FIELD_TYPE.TEXT,
+        AS: 'content',
+      },
+      '$.pier': {
+        type: SCHEMA_FIELD_TYPE.TAG,
+        AS: 'pier',
+      },
+      '$.embedding': {
+        type: SCHEMA_FIELD_TYPE.VECTOR,
+        TYPE: 'FLOAT32',
+        ALGORITHM: SCHEMA_VECTOR_FIELD_ALGORITHM.FLAT,
+        DISTANCE_METRIC: 'COSINE',
+        DIM: 768,
+        AS: 'embedding',
+      },
+    },
+    {
+      ON: 'JSON',
+      PREFIX: pierEmbeddingsPrefix,
+    })
+  } catch (e) {
+    log(e)
+    throw new Error('Redis Create Index failed.', { cause: e })
+  }
+  return result
+}
+
+async function scan() {
   const scanArgs = {
     CURSOR: '0',
-    MATCH: keyPrefix,
+    MATCH: keyPrefixStar,
     TYPE: options.keyType,
     COUNT: options.keyCount,
   }
@@ -67,23 +135,15 @@ async function del() {
     // batch.value.forEach((k) => {
     for await (const k of batch.value) {
       const pier = await redis.json.get(k)
-      const prop = pier.property
-      const addr = pier.property.address
-      // const estateNames = pier.owners.map((o) => {
-      //   if (o.estateName !== '') {
-      //     return o.estateName
-      //   } else {
-      //     return '##' 
-      //   }
-      // })
+      // const property = pier.property
+      const street = pier.property.address.street || '<ADDR>'
       const lines = pier.owners.map((o) => {
-        return `${o.estateName || '##'}: `
-          + `${o.members.map((m) => { return `${m.f} ${m.l}` }).join(', ')}`
+        return `${o.estateName || '<ESTA>'}: `
+          + `${o.members.map((m) => {
+            return (m.hidden === 0) ? `${m.f} ${m.l}` : '<UNKN>'
+          }).join(', ')}`
       }).join(', ')
-      // const owners = pier.owners.map((o) => {
-      //   return o.members.reduce
-      // })
-      const text = `pier ${pier.pier}, ${lines}`
+      const text = `pier ${pier.pier}, ${street}, ${lines}`
       console.log(text)
       count += 1
     }
@@ -92,12 +152,16 @@ async function del() {
   return count
 }
 try {
-  const result = await del()
-  log(`keys scanned ${keyPrefix}?`, result)
+  const deleteResult = await deleteVectorIndex(options.idxName)
+  const createResult = await createVectorIndex(options.idxName)
+  const scanResult = await scan()
+  log(`keys scanned ${keyPrefix}?`, scanResult)
 } catch (e) {
   error(e)
   // throw new Error(e.message, { cause: e })
   console.error(e.message)
+  console.info('Try overriding the default redis user/psswd with privileged ones.')
+  console.info('R_PRIV_USER=<user> R_PRIV_PASSWORD=<psswd> npm run createVectorSearchIndex ...')
 }
 
 // Done deleting the data, exit process.
