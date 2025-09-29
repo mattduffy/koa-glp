2a83681f787dead792ca13fd3178a9523a5da3b6
Matthew Duffy <mattduffy@gmail.com>
2025-09-28 21:25:15 -0700

    Add propertyValue.js script to use Anthropic Claude API to ask it to find property values.

---
+++ src/utils/propertyValue.js
@@ -0,0 +1,113 @@
+/**
+ * @module @mattduffy/koa-glp
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @summary The script to automate retrieving property values.
+ * @file src/utils/propertyValue.js
+ */
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'node:path'
+import * as dotenv from 'dotenv'
+import { fileURLToPath } from 'node:url'
+import Anthropic from '@anthropic-ai/sdk'
+import { Command } from 'commander'
+import { redis_single as redis } from '../daos/impl/redis/redis-single.js'
+import { _log, _error } from './logging.js'
+/* eslint-enable import/no-extraneous-dependencies */
+
+const log = _log.extend('utils:propertyValue')
+const error = _error.extend('utils:propertyValue')
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const appRoot = path.resolve(`${__dirname}/../..`)
+const appEnv = {}
+log(`appRoot: ${appRoot}`)
+dotenv.config({
+  path: path.resolve(appRoot, 'config/app.env'),
+  processEnv: appEnv,
+})
+const redisEnv = {}
+dotenv.config({
+  path: path.resolve(appRoot, 'config/redis.env'),
+  processEnv: redisEnv,
+})
+const DB_PREFIX = redisEnv.REDIS_KEY_PREFIX
+const aiEnv = {}
+dotenv.config({
+  path: path.resolve(appRoot, 'config/ai.env'),
+  processEnv: aiEnv,
+})
+log(aiEnv)
+
+const program = new Command()
+program.name('propertyValue')
+  .requiredOption('--pier-number <pier>', 'The pier number to find a property value for.')
+  .option('--dry-run', 'Dry run the command, don\'t actully change anything.')
+
+program.parse(process.argv)
+const options = program.opts()
+options.dbPrefix = DB_PREFIX
+log('options:', options)
+
+let keyPath = `${options?.keyPrefix ?? options.dbPrefix}:piers:`
+log(`full keyPath: ${keyPath}${options.pierNumber}`)
+log(`redis.options.keyPrefix: ${redis.options.keyPrefix}`)
+// process.exit()
+
+async function askClaude(address) {
+  log(aiEnv.ANTHROPIC_API_KEY)
+  const anthropic = new Anthropic({
+    apiKey: aiEnv.ANTHROPIC_API_KEY,
+  })
+  const system = 'You are a helpful agent.  '
+    + 'You can look up realestate property values using the popular website https://www.zillow.com.\n'
+    + 'Zillow calls their best estimate of a property\'s market value a Zestimate.\n'
+    + 'Include the URL to the page on the Zillow website that where you find the Zestimate.\n'
+    + 'If you are unable to find the Zestimate for the property, simply return the phrase "No Zestimate Available".'
+  const query = `I would like you to find the Zestimate for the property at this address: ${address}.`
+  log(query)
+  const msg = await anthropic.messages.create({
+    model: "claude-sonnet-4-0",
+    max_tokens: 1024,
+    system,
+    messages: [{ role: "user", content: query }],
+  })
+  return msg
+}
+
+async function pier(number) {
+  let pier 
+  if (!number) {
+    throw new Error('Missing pier number.')
+  }
+  try {
+    pier = await redis.json.get(number)
+    log(pier)
+  } catch (e) {
+    error(e)
+    throw new Error(`Failed retreiving pier ${number}`, { cause: e })
+  }
+  return pier 
+}
+try {
+  const result = await pier(`${keyPath}${options.pierNumber}`)
+  const addr = `${result.property.address.street}, `
+    + `${result.property.address.city}, `
+    + `${result.property.address.state}, `
+    + `${result.property.address.zip}`
+  const claudeResponse = await askClaude(addr)
+  log(claudeResponse)
+  // const address = `${result.property.address.street.split(' ').join('-')}-`
+  //   + `${result.property.address.city.split(' ').join('-')}-`
+  //   + `${result.property.address.zip}`
+  // log(address)
+} catch (e) {
+  error(e)
+  // throw new Error(e.message, { cause: e })
+  error(e.message)
+  log('Try overriding the default redis user/password with ones that have more privileges.')
+  log('R_PRIV_USER=<user> R_PRIV_PASSWORD=<psswd> npm run propertyValue...')
+}
+
+// Done getting property value data, exit process.
+process.exit()
