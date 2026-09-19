a92449d130fd71b84c2b96370553322618f381f7
Matthew Duffy <mattduffy@gmail.com>
2026-09-19 10:25:22 -0700

    Add reconnectStrategy() function directly to client connection options.

--- src/session-handler.js
+++ src/session-handler.js
@@ -47,6 +47,13 @@ const redisConnOpts = {
       tls: true,
       rejectUnauthorized: false,
       ca: await fs.readFileSync(redisEnv.REDIS_CACERT),
+      reconnectStrategy: (retries, cause) => {
+        console.log(`Redis session client reconnectStrategy(${retries}, cause)`)
+        console.log(cause)
+        const jitter = Math.floor(Math.random() * 200)
+        const delay = Math.min((2 ** retries) * 50, 2000)
+        return delay + jitter
+      },
     },
   },
   nodeClientOptions: {
@@ -56,6 +63,13 @@ const redisConnOpts = {
       tls: true,
       rejectUnauthorized: false,
       ca: await fs.readFileSync(redisEnv.REDIS_CACERT),
+      reconnectStrategy: (retries, cause) => {
+        console.log(`Redis session client reconnectStrategy(${retries}, cause)`)
+        console.log(cause)
+        const jitter = Math.floor(Math.random() * 200)
+        const delay = Math.min((2 ** retries) * 50, 2000)
+        return delay + jitter
+      },
     },
   },
   sentinelRetryDelayOnFailover: 100,
